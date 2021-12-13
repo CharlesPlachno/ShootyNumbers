eb26a723f3005f8e9307a84f36092490d7afd704
Charles Plachno <charlesplachno@outlook.com>
2021-12-12 18:31:57 -0800

    Added some game mode code and nothing went wrong

--- Source/ShootyNumbers/ShootyNumbersGameMode.cpp
+++ Source/ShootyNumbers/ShootyNumbersGameMode.cpp
@@ -14,4 +14,91 @@ AShootyNumbersGameMode::AShootyNumbersGameMode()
 
 	// use our custom HUD class
 	HUDClass = AShootyNumbersHUD::StaticClass();
+
+	bGameActive = true;
+	bCanRoll = true;
+	DiceRoll1 = 0;
+	DiceRoll2 = 0;
+	DiceTotal = 0;
+	TurnTotal = 0;
+	TargetsUp.Init(true, 9);
+}
+
+void AShootyNumbersGameMode::StartGame()
+{
+	bGameActive = true;
+	bCanRoll = true;
+}
+
+void AShootyNumbersGameMode::RollDice()
+{
+	if (bCanRoll)
+	{
+		DiceRoll1 = FMath::RandRange(1, 6);
+		DiceRoll2 = FMath::RandRange(1, 6);
+
+		// Figure out which targets should be available
+		DiceTotal = DiceRoll1 + DiceRoll2;
+		TurnTotal = 0;
+		bCanRoll = false;
+	}
+
+}
+
+
+// Given a target number, set the corresponding value in targets array to true
+void AShootyNumbersGameMode::SetHitTarget(int32 TargetNumber)
+{
+	int32 Index = TargetNumber - 1;
+	// Check if this target has already been hit
+	if (!TargetsUp[Index])
+	{
+		// If not, set it to true
+		TargetsUp[Index] = true;
+		UE_LOG(LogTemp, Warning, TEXT("Target hit"));
+
+		// Add Target Number to turn total
+		TurnTotal += TargetNumber;
+
+		// Check if hit this turn target value is equal or over total
+		if (TurnTotal == DiceTotal)
+		{
+			TurnOver();
+		}
+		else if (TurnTotal > DiceTotal)
+		{
+			Lose();
+		}
+	}
+}
+void AShootyNumbersGameMode::Win()
+{
+
+}
+
+void AShootyNumbersGameMode::Lose()
+{
+
+}
+
+void AShootyNumbersGameMode::TurnOver()
+{
+
+	// Check if Game Won
+	if (CheckAllTargets())
+	{
+		Win();
+	}
+}
+
+bool AShootyNumbersGameMode::CheckAllTargets()
+{
+	//for (bool TargetState : TargetsUp)
+	//{
+	//	if (!TargetState)
+	//	{
+	//		return false;
+	//	}
+	//}
+	return true;
 }
--- Source/ShootyNumbers/ShootyNumbersGameMode.h
+++ Source/ShootyNumbers/ShootyNumbersGameMode.h
@@ -13,6 +13,47 @@ class AShootyNumbersGameMode : public AGameModeBase
 
 public:
 	AShootyNumbersGameMode();
+
+	// Game Status Variables
+
+	// False when the game ends by win or loss, true when it is started
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "GameStatus")
+		bool bGameActive;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "GameStatus")
+		bool bCanRoll;
+
+	// Bool Array to represent which Targets are not hit. Game is won when all are false
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "GameStatus")
+		TArray<bool> TargetsUp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Dice)
+		int32 DiceRoll1;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Dice)
+		int32 DiceRoll2;
+
+	int32 DiceTotal;
+
+	int32 TurnTotal;
+
+	// Game Functions
+
+	UFUNCTION(BlueprintCallable)
+		void StartGame();
+
+	UFUNCTION(BlueprintCallable)
+		void RollDice();
+
+	void SetHitTarget(int32 TargetNumber);
+
+	void Win();
+
+	void Lose();
+
+	void TurnOver();
+
+	bool CheckAllTargets();
 };
 
 
